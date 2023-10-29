2cfde76a432a8c34245d76fbd055602a2967e7a3
Roman Pavlenko <rpavlenko.mail@gmail.com>
2023-10-29 21:34:30 +0100

    feat: add mobile markup

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,17 @@ for (let i = 0; i < accTab.length; i++) {
   accTab[i].addEventListener('click', function () {
     this.classList.add('active');
     let accTabContent = this.nextElementSibling;
+
     const hideContent = () => {
       accTabContent.style.maxHeight = null;
       this.classList.remove('active');
     };
+
     const showContent = () => {
       accTabContent.style.maxHeight = accTabContent.scrollHeight + 'px';
       this.classList.add('active');
     };
+
     if (accTabContent.style.maxHeight) {
       hideContent();
     } else {
